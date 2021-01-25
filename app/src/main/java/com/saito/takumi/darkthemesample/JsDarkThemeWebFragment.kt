c78d66658e1e9fcf9641552af130be4ec6e7a073
takumi-saito <takumi.saitoh.1992@gmail.com>
2021-01-26 04:53:19 +0900

    Apply Dark Theme with Javascript

--- app/src/main/java/com/saito/takumi/darkthemesample/JsDarkThemeWebFragment.kt
+++ app/src/main/java/com/saito/takumi/darkthemesample/JsDarkThemeWebFragment.kt
@@ -1,45 +1,50 @@
 package com.saito.takumi.darkthemesample
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import androidx.webkit.WebSettingsCompat
 import androidx.webkit.WebViewFeature
 
 
-class DarkThemeWebFragment : Fragment() {
+class JsDarkThemeWebFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dark_theme_web, container, false)
+        return inflater.inflate(R.layout.fragment_js_dark_theme_web, container, false)
     }
 
+    @SuppressLint("JavascriptInterface")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val webView = view.findViewById<WebView>(R.id.web_view)
         // WebView呼び出し
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("file:///android_asset/dark.html")
+        webView.loadUrl("file:///android_asset/js_dark.html")
         webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(MyJavaScript(requireContext()), "android")
+        webView.setBackgroundColor(Color.TRANSPARENT) // ダークモードのloadで白のチラつきを回避
+    }
 
+}
+class MyJavaScript(private val context: Context) {
+    @JavascriptInterface
+    fun isNightMode(): Boolean {
+        var nightMode = false
         if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
-            val nightMode = (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
-            val forceDarkMode = if (nightMode) WebSettingsCompat.FORCE_DARK_ON else WebSettingsCompat.FORCE_DARK_OFF
-            WebSettingsCompat.setForceDark(webView.settings, forceDarkMode)
+            nightMode = (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
         }
-
-        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK_STRATEGY)) {
-            WebSettingsCompat.setForceDarkStrategy(
-                webView.settings, WebSettingsCompat.DARK_STRATEGY_WEB_THEME_DARKENING_ONLY
-            )
-        }
+        return nightMode
     }
 }
