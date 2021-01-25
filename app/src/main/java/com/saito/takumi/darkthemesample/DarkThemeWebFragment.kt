038dd9f9157e2b51d28482723767da5f476faae5
takumi-saito <takumi.saitoh.1992@gmail.com>
2021-01-26 03:59:07 +0900

    rename

--- app/src/main/java/com/saito/takumi/darkthemesample/DarkThemeWebFragment.kt
+++ app/src/main/java/com/saito/takumi/darkthemesample/DarkThemeWebFragment.kt
@@ -1,15 +1,19 @@
 package com.saito.takumi.darkthemesample
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
 
 
-class WebFragment : Fragment() {
+class DarkThemeWebFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,5 +30,17 @@ class WebFragment : Fragment() {
         webView.webViewClient = WebViewClient()
         webView.loadUrl("file:///android_asset/dark.html") // ローカルのhtmlファイルを指定
         webView.settings.javaScriptEnabled = true
+
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            val nightMode = (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+            val forceDarkMode = if (nightMode) WebSettingsCompat.FORCE_DARK_ON else WebSettingsCompat.FORCE_DARK_OFF
+            WebSettingsCompat.setForceDark(webView.settings, forceDarkMode)
+        }
+
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK_STRATEGY)) {
+            WebSettingsCompat.setForceDarkStrategy(
+                webView.settings, WebSettingsCompat.DARK_STRATEGY_WEB_THEME_DARKENING_ONLY
+            )
+        }
     }
 }
